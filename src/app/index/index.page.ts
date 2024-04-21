7886873792bd155fabf846d37f30f8e96bf1630f
Angel Gonzalez <angel.gonzalezh08@gmail.com>
2024-04-21 03:48:33 -0600

    Mejora de estilos y acomodo footer

--- src/app/index/index.page.ts
+++ src/app/index/index.page.ts
@@ -51,6 +51,16 @@ export class IndexPage implements OnInit, OnDestroy {
     }
   ];
 
+  socialLinks = [
+    { url: 'https://www.facebook.com/leonix6408', title: 'Facebook', icon: 'facebook', description: 'Leonix64' },
+    { url: 'https://twitter.com/Leonix6408', title: 'Twitter', icon: 'twitter', description: 'Leonix6408' },
+    { url: 'https://www.instagram.com/leonix6408/', title: 'Instagram', icon: 'instagram', description: 'leonix64' },
+    { url: 'https://discord.gg/FW9Hxn8DAy', title: 'Discord', icon: 'discord', description: 'leonix64' },
+    { url: 'https://github.com/Leonix64', title: 'GitHub', icon: 'github', description: 'Leonix64' },
+    { url: 'https://steamcommunity.com/id/Leonix64/', title: 'Steam', icon: 'steam', description: 'leonix64' },
+    { url: 'https://www.youtube.com/@Leonix64', title: 'YouTube', icon: 'youtube', description: '@Leonix64' },
+  ];
+
   currentDateTime: Date = new Date();
   private intervalId: any;
   githubUsername = 'Leonix64';
