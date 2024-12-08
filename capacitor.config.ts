c4080979cb89afa4fff569d9d35d5382ef578a34
Angel Gonzalez <angelgonzalezh08@gmail.com>
2024-12-07 23:02:09 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LeonixPage',
+  webDir: 'www'
+};
+
+export default config;
